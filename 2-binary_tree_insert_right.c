86d6a9252096a51221fb54f3056f44f0d99b49e9
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-08 04:58:08 -0800

    fix betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,14 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_left - Creates new node for binary tree.
+ *
+ * @parent: Parent of the new node. If null new node
+ * is the parent.
+ * @value: Value of new node.
+ *
+ * Return: Pointer to the new node. On failure NULL.
+ * Failure can be memory allocation fail.
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
--- 2-main.c
+++ 2-main.c
@@ -17,7 +17,9 @@ int main(void)
     binary_tree_print(root);
     printf("\n");
     binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root->left, 53);
     binary_tree_insert_right(root, 128);
+    binary_tree_insert_right(root, 129);
     binary_tree_print(root);
     return (0);
 }
