c40d0ccedce6d3b37707e18dbed4e7293efefd21
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-08 05:04:21 -0800

    fix the else error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -30,7 +30,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right = parent->right;
 	}
 	else
-		parent->right = NULL;
+		new_node->right = NULL;
 	parent->right = new_node;
 	return (new_node);
 }
