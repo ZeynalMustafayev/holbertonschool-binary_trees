c00e39b9705162bad95ac0f56dc2c431c2003748
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-11 06:16:43 -0800

    forget delete comments

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,9 +8,6 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	//binary_tree_t parent = node->parent;
-	//binary_tree_t grandparent = parent->parent;
-
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
