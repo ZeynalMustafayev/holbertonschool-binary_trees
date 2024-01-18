6d2cc8086a6ecf5b34bd79987892e7e2b06b04de
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-18 03:50:15 -0800

    ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,7 +6,8 @@
  *
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+		const binary_tree_t *second)
 {
 	binary_tree_t *par1, *par2;
 
@@ -16,8 +17,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 		return ((binary_tree_t *)first);
 	par1 = first->parent, par2 = second->parent;
 	if (first == par2 || !par1 || (!par1->parent && par2))
-		return binary_trees_ancestor(first, par2);
+		return (binary_trees_ancestor(first, par2));
 	else if (par1 == second || !par2 || (!par2->parent && par1))
-		return binary_trees_ancestor(par1, second);
-	return binary_trees_ancestor(par1, par2);
+		return (binary_trees_ancestor(par1, second));
+	return (binary_trees_ancestor(par1, par2));
 }
